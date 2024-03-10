83bc8e47d3de84096bbe9cab490cac9c79e8ee0c
adp <adpande@gmail.com>
2024-03-11 01:54:27 +0530

    added harpoon

--- lua/abhishek.lua
+++ lua/abhishek.lua
@@ -55,3 +55,41 @@ vim.keymap.set('n', '<leader>ld', builtin.lsp_definitions, { desc = '[l]sp [D]ef
 
 -- Telescope file browser
 vim.api.nvim_set_keymap('n', '<leader>fb', ':Telescope file_browser path=%:p:h select_buffer=true<CR>', { desc = '[F]ile [B]rowser', noremap = true })
+
+-- Harpoon setup
+local harpoon = require 'harpoon'
+
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
+
+vim.keymap.set('n', '<leader>a', function()
+  harpoon:list():append()
+end)
+vim.keymap.set('n', '<C-e>', function()
+  harpoon.ui:toggle_quick_menu(harpoon:list())
+end)
+
+vim.keymap.set('n', '1', function()
+  harpoon:list():select(1)
+end)
+vim.keymap.set('n', '2', function()
+  harpoon:list():select(2)
+end)
+vim.keymap.set('n', '3', function()
+  harpoon:list():select(3)
+end)
+vim.keymap.set('n', '4', function()
+  harpoon:list():select(4)
+end)
+vim.keymap.set('n', '5', function()
+  harpoon:list():select(4)
+end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set('n', '<C-S-P>', function()
+  harpoon:list():prev()
+end)
+vim.keymap.set('n', '<C-S-N>', function()
+  harpoon:list():next()
+end)
